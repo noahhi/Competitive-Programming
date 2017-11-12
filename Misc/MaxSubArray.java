8d6eb1b693bfc4de095a56a60aaf275d647e5b0a
Noah Hunt-Isaak <huntisan@dickinson.edu>
2017-11-11 21:14:04 -0500

    fix

--- Misc/MaxSubArray.java
+++ Misc/MaxSubArray.java
@@ -1,24 +1,36 @@
+package learning;
 
-//Optimal Solution to maximum sub array problem 
+
+//Optimal Solution to maximum sub array problem. O(n)  
 
 public class KadanesAlgo {
 
 	public static void main(String[] args) {
-
 		int[] arr = {5, -2, 4, -1 ,3};
-		System.out.print(kadane(arr));
-		
+		kadane(arr);
 	}
 	
-	public static int kadane(int[] arr){
-		int globalMax = arr[0];
-		int currMax = arr[0];       		//Keeps track of max sub array ENDING AT INDEX i
+	public static void kadane(int[] arr){
+		int globalMax, globalMaxStart, globalMaxEnd, currMax, currStart;
+		globalMax = currMax = arr[0];
+		currStart = globalMaxStart = globalMaxEnd = 0;
+		
 		for(int i = 1; i < arr.length; i++){
-			currMax = Math.max(arr[i], currMax+arr[i]);
+			if(currMax + arr[i] > arr[i]){
+				currMax = currMax + arr[i];
+			} else {
+				currStart = i; 
+				currMax = arr[i];
+			}
+			
 			if(currMax > globalMax){
 				globalMax = currMax;
+				globalMaxStart = currStart;
+				globalMaxEnd = i; 
 			}
 		}
-		return globalMax;
+		System.out.println("Max SubArray Value : " + globalMax);
+		System.out.println("Start Index : " + globalMaxStart);
+		System.out.println("End Index : " + globalMaxEnd);
 	}
 }
