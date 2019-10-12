555bcc2bd811d5ca6002f98c12191d7e1b7beb11
Noah Hunt-Isaak <huntisan@dickinson.edu>
2019-10-11 22:40:50 -0400

    Update LongestSubstringWithoutRepeatingChars.java

--- LeetCode/LongestSubstringWithoutRepeatingChars.java
+++ LeetCode/LongestSubstringWithoutRepeatingChars.java
@@ -22,4 +22,23 @@ class Solution {
         }
         return best;
     }
+    
+    // better solution (O(n))
+    public int lengthOfLongestSubstring(String s) {
+        int i, j, max;
+        i = j = max = 0;
+        
+        HashMap<Character,Integer> lastSeen = new HashMap();
+        
+        while(j < s.length()){
+            char c = s.charAt(j);
+            if (lastSeen.containsKey(c) && (lastSeen.get(c) >= i)){
+                i = lastSeen.get(c)+1;
+            }
+            lastSeen.put(c, j);
+            j++;
+            if ((j-i) > max) max = j-i;
+        }
+        return max;
+    }
 }
